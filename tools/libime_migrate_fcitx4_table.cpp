e179004bd5368bc3039d0e586d9b84d99cb1f73e
liuwenhao <liuwenhao@uniontech.com>
2021-02-20 17:09:46 +0800

    gcc 8.3 also need to use boost

--- tools/libime_migrate_fcitx4_table.cpp
+++ tools/libime_migrate_fcitx4_table.cpp
@@ -18,7 +18,7 @@
 #include <istream>
 #include <sstream>
 
-#if __GNUC__ <= 7
+#if __GNUC__ <= 8
 #include <boost/filesystem.hpp>
 #else
 #include <filesystem>
@@ -88,7 +88,7 @@ struct MigrationCommonOption {
             if (useXdgPath) {
                 return stringutils::joinPath("table", path);
             } else {
-#if __GNUC__ <= 7
+#if __GNUC__ <= 8
                 return boost::filesystem::absolute(path).string();
 #else
                 return std::filesystem::absolute(path);
